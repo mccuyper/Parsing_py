e471d34f2b1e491ddc46b1a51ba537b0acf73d35
Alexandr Kononuk <mccuyper@gmail.com>
2021-08-12 00:11:15 +0300

    Update pars.py
    search github repos in news in loop by extending page using link with class 'morelink'

--- pars.py
+++ pars.py
@@ -1,14 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://news.ycombinator.com/newest'
-request = requests.get(url)
-soup = BeautifulSoup(request.text, "html.parser")
 
-titles = soup.find_all("td", class_='title')
-for title in titles:
-    title = title.find("a", {"class":'storylink'})
-    if title is not None:
-        sublink = title.get('href')
-        print(str(title.text) + "\n" + str(sublink))
-        print("=============================")
+x = 0
+while True:
+    if x == 0:
+        url = 'https://news.ycombinator.com/newest'
+    else:
+        url = 'https://news.ycombinator.com/newest'+nxt
+        
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, "html.parser")
+
+    titles = soup.find_all("td", class_='title')
+    for title in titles:
+        title = title.find("a", {"class":'storylink'})
+        if title is not None and 'github.com' in str(title):
+            sublink = title.get('href')
+            print(str(title.text) + "\n" + str(sublink))
+            print("=============================")
+
+
+    next = soup.find(class_="morelink")
+    nextlink = next.get("href")
+
+    nxt = nextlink[6:]
+    x += 1
